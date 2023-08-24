fade9602da47240684ae034d493d9cb5b7a3f2c4
arubamasjid <arubamasjid@gmail.com>
2023-08-24 01:45:41 -0400

    removed image src preventing build

--- components/content/card.vue
+++ components/content/card.vue
@@ -2,11 +2,6 @@
   <div>
     <div class="chat-notification">
       <div class="chat-notification-logo-wrapper">
-        <img
-          class="chat-notification-logo"
-          src="/img/logo.svg"
-          alt="ChitChat Logo"
-        />
       </div>
       <div class="chat-notification-content">
         <h4 class="chat-notification-title">ChitChat</h4>
