24993a108da56e0bc84ab975348e4ba03cc25274
arubamasjid <arubamasjid@gmail.com>
2023-08-29 18:04:24 -0400

    calendar

---
+++ components/calendar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>asd</div>
+</template>
+
+<script setup>
+// useHead({
+//   script: [{ src: "adhan.umd.min.js" }],
+// });
+
+import { Coordinates, CalculationMethod, PrayerTimes } from "adhan";
+
+const coordinates = new Coordinates(12.510052, -70.009354);
+const params = CalculationMethod.MoonsightingCommittee();
+const date = new Date(2023, 12, 1);
+const prayerTimes = new PrayerTimes(coordinates, date, params);
+
+console.log(prayerTimes);
+</script>
+
+<style module></style>
--- components/nav/header.vue
+++ components/nav/header.vue
@@ -1,29 +1,64 @@
 <template>
-  <div :class="$style.header" class="hidden sm:flex sm:justify-between">
+  <div :class="$style.header" class="hidden sm:flex pt-sm:justify-between">
     <div
-      class="hover:border-primary hover:border-b-4 hover:-mb-[4px] hover:scale-125 transition-transform"
+      class="hover:-mb-[4px] hover:scale-125 transition-transform text-center"
     >
-      Home
+      <NuxtLink to="/">
+        <span
+          class="xl:pl-[3vw] xl:pr-[3vw] pt-2 pb-2 hover:border-primary hover:border-b-4"
+          >Home</span
+        >
+      </NuxtLink>
     </div>
     <div
-      class="hover:border-primary hover:border-b-4 hover:-mb-[4px] hover:scale-125 transition-transform"
+      class="hover:-mb-[4px] hover:scale-125 transition-transform text-center"
     >
-      Events
+      <NuxtLink to="/events">
+        <span
+          class="xl:pl-[3vw] xl:pr-[3vw] pt-2 pb-2 hover:border-primary hover:border-b-4"
+          >Events</span
+        >
+      </NuxtLink>
     </div>
     <div
-      class="hover:border-primary hover:border-b-4 hover:-mb-[4px] hover:scale-125 transition-transform"
+      class="hover:-mb-[4px] hover:scale-125 transition-transform text-center"
     >
-      Learn
+      <NuxtLink to="/education">
+        <span
+          class="xl:pl-[3vw] xl:pr-[3vw] pt-2 pb-2 hover:border-primary hover:border-b-4"
+          >Learn
+        </span>
+      </NuxtLink>
     </div>
     <div
-      class="hover:border-primary hover:border-b-4 hover:-mb-[4px] hover:scale-125 transition-transform"
+      class="hover:-mb-[4px] hover:scale-125 transition-transform text-center"
     >
-      Donate
+      <NuxtLink to="/charity/admin">
+        <span
+          class="xl:pl-[3vw] xl:pr-[3vw] pt-2 pb-2 hover:border-primary hover:border-b-4"
+          >Charity</span
+        >
+      </NuxtLink>
     </div>
     <div
-      class="hover:border-primary hover:border-b-4 hover:-mb-[4px] hover:scale-125 transition-transform"
+      class="hover:-mb-[4px] hover:scale-125 transition-transform text-center"
     >
-      Login
+      <NuxtLink to="/contact">
+        <span
+          class="xl:pl-[3vw] xl:pr-[3vw] pt-2 pb-2 hover:border-primary hover:border-b-4"
+          >Contact</span
+        >
+      </NuxtLink>
+    </div>
+    <div
+      class="hover:-mb-[4px] hover:scale-125 transition-transform text-center hover:cursor-pointer"
+    >
+      <NuxtLink to="/auth/login">
+        <span
+          class="xl:pl-[3vw] xl:pr-[3vw] pt-2 pb-2 hover:border-primary hover:border-b-4"
+          >Login</span
+        >
+      </NuxtLink>
     </div>
   </div>
 </template>
@@ -34,11 +69,13 @@ console.log("header rendered");
 
 <style module>
 .header {
-  padding: 2rem 2rem 2rem 20vw;
+  padding: 2rem 2rem 2rem 2rem;
 }
 
 .header > div {
   /* outline: 1px solid black; */
   padding: 0.5rem 3vw 0.5rem 3vw;
+  z-index: 10;
+  width: 100%;
 }
 </style>
